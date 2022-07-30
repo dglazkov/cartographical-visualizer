39ee415e6dabd44df8bc4e749631c8a9125e8d84
Dimitri Glazkov <dimitri@glazkov.com>
2022-07-29 20:19:01 -0700

    More cleanup.

--- customer-vendor-loop.js
+++ customer-vendor-loop.js
@@ -1,6 +1,4 @@
 import { LitElement, html, svg, css } from 'https://cdn.jsdelivr.net/gh/lit/dist@2.2.7/all/lit-all.min.js'
-// import * as d3 from "https://cdn.skypack.dev/d3@7";
-
 
 class CustomerVendorLoop extends LitElement {
   static styles = css`
@@ -19,8 +17,7 @@ class CustomerVendorLoop extends LitElement {
     
     g {
       transform: scale(0.5) translate(150px, 150px);
-    }
-    `;
+    }`;
 
   // static properties = {
   //   name: {type: String},
@@ -43,7 +40,7 @@ class CustomerVendorLoop extends LitElement {
         <circle class="stock" cx="255" cy="255" r="60" />
         <circle class="stock" cx="45" cy="255" r="60" />
       </g>
-</svg>`;
+    </svg>`;
   }
 }
 
@@ -58,8 +55,7 @@ class CircleElement extends LitElement {
     height: 100%;
     align-items: center;
     text-align: center;
-  }
-  `;
+  }`;
 
   constructor() {
     super();
