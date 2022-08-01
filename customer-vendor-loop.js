3dbe9206a38413a7ae1add0dc718e8b49fd99396
Dimitri Glazkov <dimitri@glazkov.com>
2022-07-31 20:23:37 -0700

    Teaching oc-canvas links.

--- customer-vendor-loop.js
+++ customer-vendor-loop.js
@@ -1,27 +1,61 @@
-import { LitElement, html, css } from 'https://cdn.jsdelivr.net/gh/lit/dist@2.2.7/all/lit-all.min.js';
-import { forceSimulation, forceManyBody, forceCenter, forceCollide } from 'https://cdn.skypack.dev/d3-force@3';
+import { LitElement, html, svg, css } from 'https://cdn.jsdelivr.net/gh/lit/dist@2.2.7/all/lit-all.min.js';
+import {
+  forceSimulation,
+  forceManyBody,
+  forceCenter,
+  forceCollide,
+  forceLink
+} from 'https://cdn.skypack.dev/d3-force@3';
 
-const ITEM_SIZE = css`100px`;
+const ITEM_RADIUS = 50;
+const ITEM_SIZE_CSS = css`${ITEM_RADIUS*2}px`;
+const ITEM_RADIUS_CSS = css`${ITEM_RADIUS}px`;
 
 class CanvasElement extends LitElement {
+  static styles = css`
+    svg {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      transform: translate(${ITEM_RADIUS_CSS}, ${ITEM_RADIUS_CSS});
+    }
+  `;
+
   static properties = {
-    links: { type: Array }
+    _links: { type: Array }
   }
 
   constructor() {
     super();
+    this._links = [];
   }
 
   nodeSlotChanged(event) {
-    forceSimulation(event.target.assignedElements())
-      .force('manyBody', forceManyBody().strength(-20))
+    const nodes = event.target.assignedElements();
+    this._links = [];
+    nodes.forEach(node => {
+      node.hires.forEach(hireLink => {
+        this._links.push({
+          source: node.id,
+          target: hireLink
+        });
+      })
+    });
+    console.log('links', this._links);
+    forceSimulation(nodes)
+      .force('manyBody', forceManyBody().strength(-300))
       .force('center', forceCenter().x(400).y(300))
-      .force('collide', forceCollide(50));
+      .force('collide', forceCollide(100))
+      .force('links', forceLink(this._links).id(d => d.id))
+      .on('tick', () => this.requestUpdate())
   }
 
   render() {
     return html`
-    <slot name="links"></slot>
+    <svg>
+      ${this._links.map(link => svg`
+        <path d="M ${link.source.x} ${link.source.y} L ${link.target.x} ${link.target.y}" stroke="grey"></path>`)}
+    </svg>
     <slot @slotchange=${this.nodeSlotChanged} name="nodes"></slot>
     `;
   }
@@ -31,7 +65,7 @@ class ProductElement extends LitElement {
   static styles = css`
   :host {
     position: absolute;
-    width: ${ITEM_SIZE};
+    width: ${ITEM_SIZE_CSS};
   }
   
   div {
@@ -45,14 +79,21 @@ class ProductElement extends LitElement {
   static properties = {
     x: { type: Number },
     y: { type: Number },
-    name: { type: String },
+    hires: {
+      converter: {
+        fromAttribute: (value, type) => {
+          return value.split(' ');
+        }
+      }
+    },
   }
 
   constructor() {
     super();
+    this.hires = [];
     this.x = 0;
     this.y = 0;
-}
+  }
 
   render() {
     return html`
@@ -64,39 +105,18 @@ class ProductElement extends LitElement {
     <svg viewBox="0 0 100 100">
       <circle cx="50" cy="50" r="50" />
       <foreignObject x="15" y="15" width="70" height="70">
-        <div><slot></slot></div>
+        <div><span><slot></slot></span></div>
       </foreignObject>
     </svg>`;
   }
 }
 
-class LinkElement extends LitElement {
-  static properties = {
-    source: { type: String },
-    target: { type: String },
-    type: { type: String },
-  }
-
-  static styles = css`
-  :host {
-    position: absolute;
-    width: 100%;
-  }
-  `;
-
+class PlatformElement extends ProductElement {
   constructor() {
     super();
-  }
-
-  render() {
-    return html`
-    <svg viewBox="0 0 100 100">
-      <path d="M 0 0 L 100 100" stroke="gray" stroke-width="1"></path>
-    </svg>
-    `;
   }
 }
 
 customElements.define('oc-product', ProductElement);
+customElements.define('oc-platform', PlatformElement);
 customElements.define('oc-canvas', CanvasElement);
-customElements.define('oc-link', LinkElement);
